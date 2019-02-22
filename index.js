5d9dec5d00be127d81c488eb1f1aa0c7364eeb7c
Daniel Tomosovich <daniel@tomosovich.com>
2019-02-22 15:18:59 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express'); // Imports express from CommonJS modules
+
+const app = express();
+
+// Route Handler for Index
+app.get('/', (req, res) => { // Express listens for requests to '/'
+  res.send({ hi: 'there' }); // if the request matches the '/' route, {hi: 'there'} is returned
+});
+
+// Looks for env variables from Heroku, default to 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT); // tells the app to only listen to traffic over port 5000 of the http protocol
---
+++ readme.md
@@ -0,0 +1,5 @@
+1. git init
+2. git add .
+3. git commit -m "initial commit"
+4. > git remote add origin https://github.com/<repo owner>/<repo name>.git
+> git push -u origin master
